050e5bc381a8345f7686b369d6bbbd88b38c4b9e
Hulusi Demir <hulusidemir89@yahoo.com>
2018-08-27 22:29:29 +0300

    son güncellemeler

---
+++ helper/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+module.exports = ()=> {
+    mongoose.connect('mongodb://hulusi:134679aa@ds235302.mlab.com:35302/movie-api', { useNewUrlParser: true })
+}
+
+mongoose.connection.on('open',()=> {
+    console.log('MongoDB Bağlantısı Gerçekleştirildi');
+});
+
+mongoose.connection.on('error', (err)=> {
+    console.log('MongoDB Bağlantısı Hatalı : ' + err);
+});
