df172918e36ae4f666bf32352f4894887a5f5308
Hulusi Demir <hulusidemir89@yahoo.com>
2018-08-28 02:33:41 +0300

    helper/db.js change

--- helper/db.js
+++ helper/db.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 
-module.exports = ()=> {
-    mongoose.connect('mongodb://hulusi:134679aa@ds235302.mlab.com:35302/movie-api', { useNewUrlParser: true })
+module.exports = () => {
+    mongoose.connect('mongodb://hulusi:134679aa@ds235302.mlab.com:35302/movie-api', {
+        useNewUrlParser: true
+    })
+
+
+    mongoose.connection.on('open', () => {
+        console.log('MongoDB Bağlantısı Gerçekleştirildi');
+    });
+
+    mongoose.connection.on('error', (err) => {
+        console.log('MongoDB Bağlantısı Hatalı : ' + err);
+    });
+    mongoose.Promise = global.Promise; 
 }
-
-mongoose.connection.on('open',()=> {
-    console.log('MongoDB Bağlantısı Gerçekleştirildi');
-});
-
-mongoose.connection.on('error', (err)=> {
-    console.log('MongoDB Bağlantısı Hatalı : ' + err);
-});
