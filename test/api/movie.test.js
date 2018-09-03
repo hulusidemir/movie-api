7fe6fa12c277400fa75410f301da2f248ed54953
Hulusi Demir <hulusidemir89@yahoo.com>
2018-09-03 13:03:46 +0300

    test get movies

--- test/api/movie.test.js
+++ test/api/movie.test.js
@@ -29,30 +29,4 @@ describe('/api/movie Tests', () => {
 
         });
     });
-
-    describe('/POST Movie',()=> {
-        it('it should be post a movie',(done)=> {
-            const movie = {
-                title : 'Testimizin Testi',
-                category: 'Testimizin Kategorisi',
-                year: 1980,
-                imdb_score: 10,
-                director_id: 2342423
-            }
-            chai.request(server)
-            .post('/api/movie')
-            .send(movie)
-            .set('x-access-token',token)
-            .end((err,res)=> {
-                res.should.have.status(200);
-                res.body.should.be.a('object');
-                res.body.should.have.property('title');
-                res.body.should.have.property('category');
-                res.body.should.have.property('year');
-                res.body.should.have.property('imdb_score');
-                done();
-            });
-
-        });
-    });
 });
